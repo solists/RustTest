8553c95df917b4a4497d2b2149b04025fdf9aa7b
Semen09 <999solom.s.v.999@gmail.com>
2020-03-30 16:58:35 +0300

    Tested simple triangle rendering functioanlity

--- vulkano_test/src/main.rs
+++ vulkano_test/src/main.rs
@@ -32,12 +32,22 @@ use vulkano::image::Dimensions;
 use vulkano::image::StorageImage;
 use vulkano::format::ClearValue;
 
+// For graphics
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::framebuffer::Subpass;
+use vulkano::command_buffer::DynamicState;
+use vulkano::pipeline::viewport::Viewport;
+
 // Cgmath for testing purposes
 use cgmath::Vector3;
 use cgmath::prelude::*;
 
 // For profiling
 use std::time::{Duration, Instant};
+
+use image::ImageBuffer;
+use image::Rgba;
+use vulkano::framebuffer::Framebuffer;
 
 
 
@@ -125,7 +135,7 @@ fn main() {
                 // syntax. Specified directories have descending priorities based on their order.
                 include: [ "src/bin/shader-include/standard-shaders" ],
                 src: "
-                    #version 450
+                    #version 460
                     // Substitutes this line with the contents of the file `common.glsl` found in one of the standard
                     // `include` directories specified above.
                     // Note, that relative inclusion (`#include \"...\"`), although it falls back to standard
@@ -234,7 +244,7 @@ fn main() {
             vulkano_shaders::shader!{
                 ty: "compute",
                 src: "
-                #version 450
+                #version 460
                 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
                 layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
                 void main() {
@@ -287,6 +297,137 @@ fn main() {
     println!("Fractals succeeded");
     println!("Time in ms: {}", now.elapsed().as_millis());
 
+    // ************************************************
+
+    // Test graphical side of Vulkan API **************
+    let now = Instant::now();
+
+
+    let image = StorageImage::new(device.clone(), Dimensions::Dim2d { width: 1024, height: 1024 },
+                                  Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
+
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                             false, (0 .. 1024 * 1024 * 4).map(|_| 0u8))
+                                             .expect("failed to create buffer");
+
+    let p1 = cgmath::Point2 {x: -0.5, y: -0.5};
+    let p2 = cgmath::Point2 {x: 0., y: 0.5};
+    let p3 = cgmath::Point2 {x: 0.5, y: -0.25};
+
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), 
+                                                false, vec![p1, p2, p3].into_iter()).unwrap();
+
+    // Not working with cgmath for now, vertex instead
+    #[derive(Default, Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    vulkano::impl_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::vertex_buffer(),
+                                                    false, vec![vertex1, vertex2, vertex3].into_iter()).unwrap();
+
+
+    let render_pass = Arc::new(vulkano::single_pass_renderpass!(device.clone(),
+        attachments: {
+            color: {
+                load: Clear,
+                store: Store,
+                format: Format::R8G8B8A8Unorm,
+                samples: 1,
+            }
+        },
+        pass: {
+            color: [color],
+            depth_stencil: {}
+        }
+    ).unwrap());
+
+    let framebuffer = Arc::new(Framebuffer::start(render_pass.clone())
+        .add(image.clone()).unwrap()
+        .build().unwrap());
+
+    mod vs {
+        vulkano_shaders::shader!{
+            ty: "vertex",
+            src:"
+            #version 460
+            layout(location = 0) in vec2 position;
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+            "
+        }
+    }
+    mod fs {
+        vulkano_shaders::shader!{
+            ty: "fragment",
+            src:"
+            #version 450        
+            layout(location = 0) out vec4 f_color;
+            void main() {
+                f_color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+            "
+        }
+    }
+
+    let vs = vs::Shader::load(device.clone()).expect("failed to create shader module");
+    let fs = fs::Shader::load(device.clone()).expect("failed to create shader module");
+
+    let pipeline = Arc::new(GraphicsPipeline::start()
+    // Defines what kind of vertex input is expected.
+    .vertex_input_single_buffer::<Vertex>()
+    // The vertex shader.
+    .vertex_shader(vs.main_entry_point(), ())
+    // Defines the viewport (explanations below).
+    .viewports_dynamic_scissors_irrelevant(1)
+    // The fragment shader.
+    .fragment_shader(fs.main_entry_point(), ())
+    // This graphics pipeline object concerns the first pass of the render pass.
+    .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+    // Now that everything is specified, we call `build`.
+    .build(device.clone())
+    .unwrap());
+
+    let dynamic_state = DynamicState {
+        viewports: Some(vec![Viewport {
+            origin: [0.0, 0.0],
+            dimensions: [1024.0, 1024.0],
+            depth_range: 0.0 .. 1.0,
+        }]),
+        .. DynamicState::none()
+    };
+    
+    let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family()).unwrap()
+        .begin_render_pass(framebuffer.clone(), false, vec![[0.0, 0.0, 1.0, 1.0].into()])
+        .unwrap()
+    
+        .draw(pipeline.clone(), &dynamic_state, vertex_buffer.clone(), (), ())
+        .unwrap()
+    
+        .end_render_pass()
+        .unwrap()
+
+        .copy_image_to_buffer(image.clone(), buf.clone())
+        .unwrap()
+
+        .build()
+        .unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
+
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("triangle.png").unwrap();
+
+    
+    println!("Time in ms: {}", now.elapsed().as_millis());
     // ************************************************
 
 }
